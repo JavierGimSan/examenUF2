58cb594c84e15de0afe6a3d79654689564419469
JavierGimSan <javi.gimsa@gmail.com>
2025-01-13 17:20:11 +0100

    Exercici 1

---
+++ conn.py
@@ -0,0 +1,15 @@
+import psycopg2
+
+def connection_db():
+
+    conn = psycopg2.connect(
+        database = "postgres_24278",
+        user = "user_postgres1",
+        password = "pass_postgres1",
+        host = "localhost",
+        port = "5432"
+    )
+
+    print("Connexió establerta correctament")
+    return conn
+
